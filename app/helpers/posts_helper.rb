dfe796e679901fbfb722abf7225c1573423a73d6
John Howard <longhornjoho@gmail.com>
2016-04-05 02:03:55 +0000

    assignment-31-CRUD: partial

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+    def spam_filter
+        posts = Post.all
+        posts.each_with_index do |post, index|
+            if index % 5 == 0
+                post.title = "SPAM"
+            end
+        end
+    end
 end
