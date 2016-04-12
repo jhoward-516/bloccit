08fc973d510046b69ccd52494eb0c99bf9c3f4b7
John Howard <longhornjoho@gmail.com>
2016-04-12 14:32:22 +0000

    assignment-39: complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,6 @@ class Post < ActiveRecord::Base
     validates :body, length: {minimum: 20 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
+    
+    default_scope { order('created_at DESC') }
 end
